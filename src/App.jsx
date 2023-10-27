aa390d4ec513e986210c0c8ba88b06495aa47e4b
¨jppinedal¨ <jppinedal@unal.edu.co>
2023-10-27 15:25:48 -0500

    Mi primer commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+//import { useState } from 'react'
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import { useState, useEffect } from "react";
+import "./App.css";
+import Card from "./componets/card";
+const url = "https://rickandmortyapi.com/api/character";
+
+function App() {
+  const [characters, setCaracters] = useState();
+  useEffect(() => {
+    fetch(url) // fetch es una funcion que hace una peticion a una url
+      .then((res) => res.json()) // res es la respuesta que nos da el servidor
+      .then((data) => {
+        setCaracters(data.results); // data es la informacion que nos trae el servidor
+      });
+  }, []);
+
+  console.log("characters", characters); // para ver que nos trae la api
+  return (
+    <>
+      <h1> Rick and Morty as Ruta N botcamp</h1>
+      <p> With React by Juan Pablo Pineda L</p>
+      <p>Personajes:</p>
+
+      <div className="liscar">
+        {characters?.map((cha) => (
+          <Card key={cha.id} char={cha} />
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default App;
